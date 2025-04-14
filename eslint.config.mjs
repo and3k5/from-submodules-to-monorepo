78898c1a5493ae3ee8f2ef88413d38c9dd4c9ad3
Anders K <and3k5@users.noreply.github.com>
2025-04-14 10:30:32 +0200

    Add some more eslint + typescript (for d.ts files)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,50 @@
 import { defineConfig } from "eslint/config";
-import globals from "globals";
 import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig([
-    { files: ["**/*.{js,mjs,cjs}"] },
-    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+    includeIgnoreFile(fileURLToPath(new URL(".gitignore", import.meta.url))),
     {
-        files: ["**/*.{js,mjs,cjs}"],
-        languageOptions: { globals: globals.node },
-    },
-    {
-        files: ["**/*.{js,mjs,cjs}"],
+        files: ["**/*.{js,mjs,cjs,ts}"],
         plugins: { js },
         extends: ["js/recommended"],
+    },
+    {
+        files: ["**/*.js"],
+        languageOptions: { sourceType: "commonjs" },
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        languageOptions: { globals: globals.node },
+    },
+    tseslint.config(tseslint.configs.recommended, {
+        rules: {
+            "@typescript-eslint/no-require-imports": "off",
+        },
+    }),
+    {
+        files: ["**/*.json"],
+        ignores: ["package-lock.json"],
+        plugins: { json },
+        language: "json/json",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.jsonc"],
+        plugins: { json },
+        language: "json/jsonc",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.md"],
+        plugins: { markdown },
+        language: "markdown/gfm",
+        extends: ["markdown/recommended"],
     },
 ]);
