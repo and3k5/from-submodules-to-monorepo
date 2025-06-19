a6c8bd922b1a9a528d7b43c89608aaddd3845869
Anders K <and3k5@users.noreply.github.com>
2025-06-19 23:23:36 +0200

    Ensure extracted files actually exists

--- operations/main-repo/04-unpack-archives/thread-worker.ts
+++ operations/main-repo/04-unpack-archives/thread-worker.ts
@@ -1,6 +1,6 @@
 import { workerData, isMainThread, parentPort } from "worker_threads";
 import { createConsoleWrapper } from "../../../utils/output/console-wrapper";
-import { readdirSync } from "fs";
+import { existsSync, readdirSync } from "fs";
 import { join } from "path";
 import { extract } from "tar";
 
@@ -9,12 +9,11 @@ if (!isMainThread) {
     const mainRepoDir = workerData.mainRepoDir;
 
     const console = createConsoleWrapper();
-    
+
     const files = readdirSync(keepUntrackedFilesPath).filter((file) =>
         file.endsWith(".tar.gz"),
     );
     (async function () {
-
         if (files.length === 0) {
             console.log("No tar files found in the specified directory.");
             parentPort?.postMessage(console.contents);
@@ -23,12 +22,29 @@ if (!isMainThread) {
         for (const file of files) {
             const filePath = join(keepUntrackedFilesPath, file);
             console.log(`Extracting ${filePath} to ${mainRepoDir}`);
+            const pathsToCheck: string[] = [];
             await extract({
                 file: filePath,
                 cwd: mainRepoDir,
-                onReadEntry: (entry) => console.log(`Read entry ${entry.path}`),
-                onWriteEntry: (entry) => console.log(`Write entry ${entry.path}`),
+                onReadEntry: (entry) => {
+                    pathsToCheck.push(entry.path);
+                    console.log(`Read entry ${entry.path}`);
+                },
+                onWriteEntry: (entry) => {
+                    // this shouldn't be called
+                    console.log(`Write entry ${entry.path}`);
+                },
             });
+            for (const pathToCheck of pathsToCheck) {
+                const actualPath = join(mainRepoDir, pathToCheck);
+                if (!existsSync(actualPath)) {
+                    throw new Error(
+                        `Unpack of ${file} should result in a file to exist but it didn't. File path: ${actualPath}`,
+                    );
+                }else{
+                    console.log(actualPath+" exists!");
+                }
+            }
         }
 
         console.log("All tar files have been extracted.");
